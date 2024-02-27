e26a979f0cf997569f29d684ee0df1725197e096
Daron Yondem <daron@yondem.com>
2024-02-27 11:50:11 +0300

    Update Discord workflow to install axios and use separate script for notifying Discord

---
+++ .github/workflows/notifyDiscord.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const axios = require('axios');
+
+// Ensure you have set this environment variable in your GitHub secrets
+const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
+const filePath = './content/speaking.md'; // Adjust the path as necessary
+
+fs.readFile(filePath, 'utf8', (err, markdown) => {
+  if (err) {
+    console.error('Error reading the markdown file:', err);
+    return;
+  }
+
+  const tableStart = markdown.indexOf("Here is where I'm planning to be next;") + "Here is where I'm planning to be next;".length;
+  const tableEnd = markdown.indexOf('<br/>', tableStart);
+  const tableContent = '```' + markdown.substring(tableStart, tableEnd).trim() + '```';
+
+  axios.post(webhookUrl, {
+    username: 'Etkinlik Habercisi',
+    avatar_url: 'https://cdn-icons-png.flaticon.com/512/7653/7653930.png',
+    content: tableContent
+  })
+  .then(() => console.log('Message sent'))
+  .catch(console.error);
+});
